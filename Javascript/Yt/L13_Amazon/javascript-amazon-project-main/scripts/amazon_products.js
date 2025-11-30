2959691d841a14ddcc6d6209de36fa409512ba31
Navaneetha Krishna <krishnaofficial2026@gmail.com>
2025-11-30 19:49:39 +0530

    amazon_part1

---
+++ Javascript/Yt/L13_Amazon/javascript-amazon-project-main/scripts/amazon_products.js
@@ -0,0 +1,85 @@
+let html = "";
+products.forEach((item) => {
+  html += `
+                <div class="product-container">
+                    <div class="product-image-container">
+                        <img class="product-image"
+                        src="${item.image}">
+                    </div>
+
+                    <div class="product-name limit-text-to-2-lines">
+                        ${item.name}
+                    </div>
+
+                    <div class="product-rating-container">
+                        <img class="product-rating-stars"
+                        src="images/ratings/rating-${
+                          item.rating.stars * 10
+                        }.png">
+                        <div class="product-rating-count link-primary">
+                        ${item.rating.count}
+                        </div>
+                    </div>
+
+                    <div class="product-price">
+                        $${item.priceCents / 100}
+                    </div>
+
+                    <div class="product-quantity-container">
+                        <select>
+                        <option selected value="1">1</option>
+                        <option value="2">2</option>
+                        <option value="3">3</option>
+                        <option value="4">4</option>
+                        <option value="5">5</option>
+                        <option value="6">6</option>
+                        <option value="7">7</option>
+                        <option value="8">8</option>
+                        <option value="9">9</option>
+                        <option value="10">10</option>
+                        </select>
+                    </div>
+
+                    <div class="product-spacer"></div>
+
+                    <div class="added-to-cart">
+                        <img src="images/icons/checkmark.png">
+                        Added
+                    </div>
+
+                    <button class="add-to-cart-button button-primary add-to-cart-btn"  data-item-id="${
+                      item.id
+                    }">
+                        Add to Cart
+                    </button>
+                    </div>
+                `;
+});
+document.querySelector(".list-products").innerHTML = html;
+
+document.querySelectorAll(".add-to-cart-btn").forEach((button) => {
+  button.addEventListener("click", () => {
+    const pId = button.dataset.itemId;
+    let matchingItem;
+    cart.forEach((item) => {
+      if (item.id === pId) {
+        matchingId = item;
+      }
+    });
+    if (matchingItem) {
+      matchingItem.quantity++;
+    } else {
+      cart.push({
+        productId: pId,
+        quantity: 1,
+      });
+    }
+
+    let cartquantity = 0;
+    cart.forEach((item) => {
+      cartquantity += item.quantity;
+    });
+
+    document.querySelector(".cart-quantity").innerHTML = cartquantity;
+  });
+});
